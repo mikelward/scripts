85ab4609b3d05232f0ebc41e5dd4a379580d85a7
Mikel Ward <mikel@mikelward.com>
2007-10-03 02:43:14 +0000

    Make the mail message a multipart MIME message including both a text and HTML version so we can make the message more readablein text-based mail readers like Mutt while retaining the original format for HTML-capabable mail clients (Outlook, Thunderbird, etc.)

--- reportblocked.pl
+++ reportblocked.pl
@@ -14,8 +14,12 @@ my $postmaster = undef;
 my $maillog = "/var/log/mail";
 my $sendmail = "/usr/lib/sendmail";
 
-sub print_header
+my @records;
+
+sub print_html_header
 {
+	print SENDMAIL "<html>\n";
+	print SENDMAIL "<body>\n";
 	print SENDMAIL "<p>Somebody tried to send you the following messages, but they were blocked because they looked suspicious.</p>\n";
 	print SENDMAIL "<p>In each case, the person who sent the message will have received an error message informing them the message was not delivered.</p>\n";
 	print SENDMAIL "<p>If you wanted one of these messages, please contact the person who sent it.  They will need to ask their system administrator to fix the problem mentioned in the Reason column and then re-send the message.</p>\n";
@@ -26,40 +30,122 @@ sub print_header
 	print SENDMAIL "</tr>\n";
 }
 
+sub print_text_header
+{
+	print SENDMAIL "Somebody tried to send you the following messages, but they were blocked because they looked suspicious.\n\n";
+	print SENDMAIL "In each case, the person who sent the message will have received an error message informing them the message was not delivered.\n\n";
+	print SENDMAIL "If you wanted one of these messages, please contact the person who sent it.  They will need to ask their system administrator to fix the problem mentioned in the Reason column and then re-send the message.\n\n";
+	#print SENDMAIL "<p>If you have any questions, please <a href=\"mailto:$postmaster\">contact the post master.</a></p>\n";
+	printf SENDMAIL "%-7s  %-33s  %-14s  %-19s\n", "TIME", "FROM", "COUNTRY", "REASON";
+}
+
+sub print_html_body
+{
+	my($recordref) = shift(@_);
+	my(%record) = %$recordref;
+
+	print SENDMAIL "<tr>\n";
+	print SENDMAIL "<td>$record{time}</td>\n";
+	print SENDMAIL "<td><a href=\"mailto:$record{from}\">$record{from}</a></td>\n";
+	print SENDMAIL "<td>$record{country}</td>\n";
+	#print SENDMAIL "<td>$record{host}</td>\n";
+	print SENDMAIL "<td>$record{reason}</td>\n";
+	#print SENDMAIL "<td>$record{details}</td>\n";
+	print SENDMAIL "</tr>\n";
+}
+
+sub print_text_body
+{
+	my($recordref) = shift(@_);
+	my(%record) = %$recordref;
+
+	printf SENDMAIL "%-7s  %-33s  %-14s  %-19s\n", $record{time}, $record{from}, $record{country}, $record{reason};
+}
+
+sub print_html_footer
+{
+	my($statsref) = shift(@_);
+	my(%stats) = %$statsref;
+
+	print SENDMAIL "</table>\n";
+
+	if ($stats{blocked} == 0)
+	{
+		print SENDMAIL "<p>No messages blocked</p>\n";
+	}
+	elsif ($stats{blocked} == 1)
+	{
+		print SENDMAIL "<p>One message blocked</p>\n";
+	}
+	else
+	{
+		print SENDMAIL "<p>$stats{blocked} messages blocked</p>\n";
+	}
+
+	if ($stats{delivered} == 0)
+	{
+		print SENDMAIL "<p>No messages delivered</p>\n";
+	}
+	elsif ($stats{delivered} == 1)
+	{
+		print SENDMAIL "<p>One message delivered</p>\n";
+	}
+	else
+	{
+		print SENDMAIL "<p>$stats{delivered} messages delivered</p>\n";
+	}
+
+	print SENDMAIL "</body>\n";
+	print SENDMAIL "</html>\n";
+}
+
+sub print_text_footer
+{
+	my($statsref) = shift(@_);
+	my(%stats) = %$statsref;
+
+	print SENDMAIL "\n";
+
+	if ($stats{blocked} == 0)
+	{
+		print SENDMAIL "No messages blocked\n\n";
+	}
+	elsif ($stats{blocked} == 1)
+	{
+		print SENDMAIL "One message blocked\n\n";
+	}
+	else
+	{
+		print SENDMAIL "$stats{blocked} messages blocked\n\n";
+	}
+
+	if ($stats{delivered} == 0)
+	{
+		print SENDMAIL "No messages delivered\n\n";
+	}
+	elsif ($stats{delivered} == 1)
+	{
+		print SENDMAIL "One message delivered\n\n";
+	}
+	else
+	{
+		print SENDMAIL "$stats{delivered} messages delivered\n\n";
+	}
+}
+
 for my $user (@users)
 {
 	open(MAILLOG, "<$maillog")
 		or die "Cannot open mail log";
 
-	open(SENDMAIL, "|$sendmail -t")
-		or die "Cannot open sendmail pipe";
-
-	print SENDMAIL "To: $user\@$domain\n";
-	if (defined $postmaster)
-	{
-		print SENDMAIL "Bcc: $postmaster\n";
-	}
-	print SENDMAIL "Subject: Blocked Messages Report for $user\n";
-	print SENDMAIL "MIME-Version: 1.0\n";
-	print SENDMAIL "Content-Type: text/html\n";
-	print SENDMAIL "\n";
-
-	print SENDMAIL "<html>\n";
-	print SENDMAIL "<body>\n";
-
-	my $count = 0;
-	my $received = 0;
+	my($blocked) = 0;
+	my($delivered) = 0;
 	while (<MAILLOG>)
 	{
 		#if (m#^(...) (..) (........) ([^ ]*) postfix/smtpd\[\d+\]: NOQUEUE: reject: RCPT from ([^[]*)\[([^]]*)\]: (5..) ([^ ]*) (.*); from=<([^>]*)> to=<([^>]*)> proto=[^ ]* helo=#)
 		if (m#^(...) (..) (........) ([^ ]*) postfix/smtpd\[\d+\]: NOQUEUE: reject: RCPT from ([^[]*)\[([^]]*)\]: (5..) ([^ ]*) (.*); from=<([^>]*)> to=<$user(\+[^@]*)?@.*> proto=[^ ]* helo=<.*>#i)
 		{
-			$count++;
-
-			if ($count == 1)
-			{
-				&print_header;
-			}
+			$blocked++;
 
 			my $month = $1;
 			my $day = $2;
@@ -108,9 +194,12 @@ for my $user (@users)
 			$time = "$hours:$minutes$timesuffix";
 
 			my $country = `geoiplookup $addr`;
+			chomp($country);
 			$country =~ s/^GeoIP Country Edition: //;
-			$country =~ s/^.*?,//;	# strip country code
+			$country =~ s/^.*?, //;	# strip country code
 			$country =~ s/,.*$//;	# strip things like "Republic of"
+
+			print "country is x${country}x\n";
 
 			my $details;
 			if ($reason =~ /Your mail server is blacklisted by ([^ ]*)\. /)
@@ -154,6 +243,10 @@ for my $user (@users)
 				$reason = "Bad To address";
 				$details = $to;
 			}
+			elsif ($reason =~ /Suspected spam/)
+			{
+				$reason = "Suspected spam";
+			}
 			elsif ($reason =~ m#Recipient address rejected: Please see http://www.openspf.org#)
 			{
 				$reason = "Forged From address";
@@ -170,55 +263,72 @@ for my $user (@users)
 				$from = "postmaster\@$host";
 			}
 
-			print SENDMAIL "<tr>\n";
-			print SENDMAIL "<td>$time</td>\n";
-			print SENDMAIL "<td><a href=\"mailto:$from\">$from</a></td>\n";
-			print SENDMAIL "<td>$country</td>\n";
-			#print SENDMAIL "<td>$host</td>\n";
-			print SENDMAIL "<td>$reason</td>\n";
-			#print SENDMAIL "<td>$details</td>\n";
-			print SENDMAIL "</tr>\n";
+			#my %record;
+			my $recordref = { time => $time, from => $from, country => $country, host => $host, reason => $reason, details => $details };
+			#$record{time} = $time;
+			#$record{from} = $from;
+			#$record{country} = $country;
+			#$record{host} = $host;
+			#$record{reason} = $reason;
+			#$record{details} = $details;
+
+			#push @records, %record;
+			push @records, $recordref;
 
 		}
 		# Jul  6 06:33:21 eagle postfix/local[11666]: 5030B3746C: to=<michael@endbracket.net>, relay=local, delay=6.6, delays=3.1/0.04/0/3.5, dsn=2.0.0, status=sent (delivered to command: /usr/local/bin/procmail -p -a "$EXTENSION")
 		elsif (m#^(...) (..) (........) ([^ ]*) postfix/local\[\d+\]:.*to=<$user(\+[^@]*)?@.*>.*status=sent#)
 		{
-			$received++;
+			$delivered++;
 		}
 	}
-	if ($count > 0)
+
+	my $statsref = { blocked => $blocked, delivered => $delivered };
+
+	open(SENDMAIL, "|$sendmail -t")
+		or die "Cannot open sendmail pipe";
+
+	print SENDMAIL "To: $user\@$domain\n";
+	if (defined $postmaster)
 	{
-		print SENDMAIL "</table>\n";
+		print SENDMAIL "Bcc: $postmaster\n";
 	}
 
-	if ($count == 0)
+	print SENDMAIL "Subject: Blocked Messages Report for $user\n";
+	print SENDMAIL "MIME-Version: 1.0\n";
+	print SENDMAIL "Content-Type: multipart/alternative; boundary=\"boundary\"\n";
+	print SENDMAIL "\n";
+	print SENDMAIL "This is a multi-part message in MIME format.\n";
+
+	print SENDMAIL "--boundary\n";
+	print SENDMAIL "Content-Type: text/plain\n";
+	print SENDMAIL "\n";
+
+
+	print_text_header();
+
+	foreach my $recordref (@records)
 	{
-		print SENDMAIL "<p>No messages blocked</p>\n";
-	}
-	elsif ($count == 1)
-	{
-		print SENDMAIL "<p>One message blocked</p>\n";
-	}
-	else
-	{
-		print SENDMAIL "<p>$count messages blocked</p>\n";
+		print_text_body($recordref);
 	}
 
-	if ($received == 0)
+	print_text_footer($statsref);
+
+
+	print SENDMAIL "--boundary\n";
+	print SENDMAIL "Content-Type: text/html\n";
+	print SENDMAIL "\n";
+
+	print_html_header();
+
+	foreach my $recordref (@records)
 	{
-		print SENDMAIL "<p>No messages delivered</p>\n";
-	}
-	elsif ($received == 1)
-	{
-		print SENDMAIL "<p>One message delivered</p>\n";
-	}
-	else
-	{
-		print SENDMAIL "<p>$received messages delivered</p>\n";
+		print_html_body($recordref);
 	}
 
-	print SENDMAIL "</body>\n";
-	print SENDMAIL "</html>\n";
+	print_html_footer($statsref);
+
+	print SENDMAIL "--boundary--\n";
 
 	close(SENDMAIL);
 }
