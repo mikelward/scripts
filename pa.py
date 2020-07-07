9f70f2c87422d21438dc47977fc4751028adacdf
Mikel Ward <mbw@google.com>
2020-07-07 11:50:19 -0700

    Also print the name of the default source's port.
    This helps make it clearer whether the headset's microphone port is active.

--- pa.py
+++ pa.py
@@ -1,34 +1,40 @@
 #!/usr/bin/env python3
-# Prints the name of the PulseAudio default sink's active port.
+# Prints the name of the PulseAudio default sink's active port and the default
+# source's active port.
 
 import re
 import subprocess
 
-result = subprocess.run(['pacmd', 'list-sinks'], capture_output=True)
 
-port_names = {}
-default = False
-ports = False
-port_re = re.compile(b'^\t\t(\S+): (.*) \(.*\)$')
-for line in result.stdout.splitlines():
-    if line.startswith(b'  * index: '):
-        default = True
-    if line.startswith(b'    index: '):
-        default = False
-    if not default:
-        continue
-    if line.startswith(b'	ports:'):
-        ports = True
-    if line.startswith(b'	active port: '):
-        active_port = line.split(b':', 1)[1].strip().strip(b'<>')
-        print(port_names[active_port].decode('utf-8'))
-        ports = False
-    if not ports:
-        continue
-    if m := port_re.match(line):
-        port_names[m[1]] = m[2]
-
+def get_active_port(output):
+  port_names = {}
+  default = False
+  ports = False
+  port_re = re.compile(b'^\t\t(\S+): (.*) \(.*\)$')
+  for line in output.splitlines():
+      if line.startswith(b'  * index: '):
+          default = True
+      if line.startswith(b'    index: '):
+          default = False
+      if not default:
+          continue
+      if line.startswith(b'	ports:'):
+          ports = True
+      if line.startswith(b'	active port: '):
+          active_port = line.split(b':', 1)[1].strip().strip(b'<>')
+          return port_names[active_port].decode('utf-8')
+      if not ports:
+          continue
+      if m := port_re.match(line):
+          port_names[m[1]] = m[2]
 
 
+def main():
+  ports = [get_active_port(result.stdout)
+           for subcommand in ['list-sinks', 'list-sources']
+           for result in [subprocess.run(['pacmd', subcommand], capture_output=True)]]
+  print(' '.join('%s' % p for p in ports))
 
 
+if __name__ == '__main__':
+  main()
